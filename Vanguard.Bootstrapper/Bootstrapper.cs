bb75f8df2d2a7e19008cf980160586f83066e5e4
Mifka Rene <rene.mifka@gmail.com>
2025-03-15 19:19:03 +0100

    add installer project

---
+++ Vanguard.Bootstrapper/Bootstrapper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace Vanguard.Bootstrapper
+{
+    public static class Bootstrapper
+    {
+        public static void Init()
+        {
+            try
+            {
+                Console.WriteLine("[Vanguard] Bootstrapper Initialized!");
+
+                var baseDir = AppDomain.CurrentDomain.BaseDirectory;
+                var modsDir = Path.Combine(baseDir, "Mods");
+                var loaderPath = Path.Combine(modsDir, "Vanguard.Loader.dll");
+
+                if (!File.Exists(loaderPath))
+                {
+                    Console.WriteLine("[Vanguard] Vanguard.Loader.dll not found at: " + loaderPath);
+                    return;
+                }
+
+                var loaderAssembly = Assembly.LoadFrom(loaderPath);
+                var entryType = loaderAssembly.GetType("Vanguard.Loader.Entry");
+                var initMethod = entryType?.GetMethod("Initialize", BindingFlags.Public | BindingFlags.Static);
+
+                if (initMethod != null)
+                {
+                    initMethod.Invoke(null, null);
+                    Console.WriteLine("[Vanguard] Vanguard.Loader initialized!");
+                }
+                else
+                {
+                    Console.WriteLine("[Vanguard] Could not find Initialize method in Loader.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[Vanguard] Bootstrapper Error: {ex}");
+            }
+        }
+    }
+}
---
+++ Vanguard.Installer/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Vanguard.Installer.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
